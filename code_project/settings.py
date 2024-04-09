ec556b312ce0a4e6639510046017e65f40af48bc
Zahid295 <zahidbhatti074@gmail.com>
2024-04-09 18:41:21 +0000

    Change settings file

--- code_project/settings.py
+++ code_project/settings.py
@@ -25,7 +25,7 @@ TEMPLATES_DIR = os.path.join(BASE_DIR, 'templates//')
 # See https://docs.djangoproject.com/en/4.2/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = 'django-insecure-b^%i-14_r!97x8cu+9cqy059s-_%0u9k$t58%jfouye)3zmpe)'
+
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
--- env.py
+++ env.py
@@ -1,5 +1,5 @@
 import os
 
 os.environ.setdefault(
-    "DATABASE_URL", "postgres://zfjicurh:KnSDnqUtoSjLyEXb5JMJMv17VdQrMtpA@flora.db.elephantsql.com/zfjicurh")
-    SECRET_KEY = 'django-insecure-b^%i-14_r!97x8cu+9cqy059s-_%0u9k$t58%jfouye)3zmpe)'
+    "DATABASE_URL", "postgres://zfjicurh:KnSDnqUtoSjLyEXb5JMJMv17VdQrMtpA@flora.db.elephantsql.com/zfjicurh",
+    SECRET_KEY = "django-insecure-b^%i-14_r!97x8cu+9cqy059s-_%0u9k$t58%jfouye)3zmpe)")
