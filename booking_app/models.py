39d9cbcecd9e387d67b2d021162a3df1add1e7f8
Zahid295 <zahidbhatti074@gmail.com>
2024-04-10 07:43:35 +0000

    Add validation for Table name

--- booking_app/models.py
+++ booking_app/models.py
@@ -1,5 +1,6 @@
 from django.contrib.auth.models import User
 from django.db import models
+from django.core.validators import MinLengthValidator
 
 # Create your models here.
 
@@ -10,7 +11,7 @@ class Table(models.Model):
         ("Cozy Corner", "Cozy Corner"),
         ("Family Booth", "Family Booth"),
     ]
-    name = models.CharField(max_length=50, choices=CHOICES, default=Window View)
+    name = models.CharField(max_length=50, choices=CHOICES, default=Window View, validators=[MinLengthValidator(1)])
 
     def __str__(self):
         return self.name
@@ -18,7 +19,7 @@ class Table(models.Model):
 class Booking(models.Model):
     table = models.ForeignKey(Table, on_delete=models.CASCADE)
     user = models.ForeignKey(User, on_delete=models.CASCADE, default=1)
-    guest_name = models.CharField(max_length=100)
+    guest_name = models.CharField(max_length=100, validators=[MinLengthValidator(1)])
     booking_date = models.DateField()
     booking_time = models.TimeField()
     is_cancelled = models.BooleanField(default=False)
