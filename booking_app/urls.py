162b891834292de9c0712d2f0ce6103f7f4e5fe2
Zahid295 <zahidbhatti074@gmail.com>
2024-05-07 17:24:07 +0000

    Add path for delete booking url

--- booking_app/urls.py
+++ booking_app/urls.py
@@ -6,4 +6,5 @@ urlpatterns = [
     path('bookings/', views.book_table, name='booking-form'),
     path('booking-details/', views.booking_details, name='booking-details'),
     path('edit-booking/<int:booking_id>/', views.edit_booking, name='edit-booking'),
+    path('delete-booking/<int:booking_id>/', views.delete_booking, name='delete-booking'),
 ]
