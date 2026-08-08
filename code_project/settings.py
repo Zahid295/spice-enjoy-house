e630f5c5dde0b230529af4bffc1be910996636fa
Zahid295 <Zahidbhatti074@gmail.com>
2026-08-08 12:26:33 +0100

    Update settings

--- code_project/settings.py
+++ code_project/settings.py
@@ -33,7 +33,7 @@ SECRET_KEY = os.getenv('SECRET_KEY')
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = False
 
-ALLOWED_HOSTS = ['127.0.0.1', 'localhost']
+ALLOWED_HOSTS = ['127.0.0.1', 'localhost', 'spice-enjoy-house-e5f9e349b39d.herokuapp.com']
 
 
 # Application definition
